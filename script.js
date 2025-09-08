461e609b7b899c0624cdcf87f1849f39bf159726
eggtoasts <129806552+eggtoasts@users.noreply.github.com>
2025-09-08 16:21:53 -0400

    Made the grid generator's code alot more shorter The new code now grabs the direct reference from creating the row element instead of inefficiently searching for the row element every time.

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
-//Make a 16x16 grid with flex box
-
-//Should have the option to change number of squares per side
-// (same size, 940px)
-//Max squares per side is 100
-
-//For loop? Calculate the div boxes by tot / inputted square, ex (940 / 100 -->) should be 9.4 x 9.4 per box
-
-//I want the user to be able to pick the colors that they want :]
-
 const gridContainer = document.querySelector(".grid-container");
 
-makeGrid(16);
+makeGrid(20);
 
 function makeGrid(size) {
   let squarePx = 16;
@@ -19,25 +9,18 @@ function makeGrid(size) {
 
   //Make a row first, then put it in each row.
 
-  const cell = document.createElement("div");
-  const square = document.createElement("div");
-
-  square.setAttribute("class", "square");
-  cell.setAttribute("class", "cell");
-
   for (let j = 0; j < size; j++) {
-    square.setAttribute("class", `square`);
-    let str = "square" + (j + 1);
-    square.classList.add(str);
+    //Made the code shorter-- realized that I can get a direct refernence from creating the object,
+    //rather than going out of my way using querySelector
+    const currRow = document.createElement("div");
+    currRow.setAttribute("class", "row");
+    gridContainer.appendChild(currRow);
 
-    gridContainer.appendChild(square.cloneNode(true));
-    console.log(str);
-
-    const currRow = document.querySelector("." + str);
     console.log(currRow);
     for (let k = 0; k < size; k++) {
-      // console.log(currRow);
-      currRow.appendChild(cell.cloneNode(true));
+      const cell = document.createElement("div");
+      cell.setAttribute("class", "cell");
+      currRow.appendChild(cell);
     }
   }
 }
