934db0fe5c01bf458a09836da9ef518a2d2da27d
eggtoasts <129806552+eggtoasts@users.noreply.github.com>
2025-09-08 17:37:06 -0400

    Added a interactive slider where the user can change the grid size of the canvas. This patch will let the user move the slider to resize the grid from 1x1 to 100x100.

--- script.js
+++ script.js
@@ -1,26 +1,58 @@
 const gridContainer = document.querySelector(".grid-container");
+const sizeSlider = document.querySelector(".slider");
+const sizeOfGridText = document.querySelector(".grid-size-text");
 
-makeGrid(20);
+//Default size.
+makeGrid(16);
+
+let sizeOfGrid = 16;
+
+sizeSlider.addEventListener("mousedown", (event) => {
+  let target = event.target;
+
+  let sizeInterval = setInterval(() => {
+    console.log("This is" + target.value);
+    updateSizeText(target.value);
+    makeGrid(target.value);
+  }, 100);
+
+  sizeSlider.addEventListener("mouseup", () => {
+    let target = event.target;
+    console.log(target.value);
+
+    clearInterval(sizeInterval);
+    updateSizeText(target.value);
+  });
+});
+
+//This will update the  text that indicates the grid's size/
+function updateSizeText(size) {
+  sizeOfGridText.textContent = `${size} x ${size}`;
+  sizeOfGrid = size;
+}
 
 function makeGrid(size) {
-  let squarePx = 16;
+  //Deletes the old grid.
+  removeGrid();
 
-  let squareSize = 960 / 16;
-
-  //Make a row first, then put it in each row.
-
+  //Makes a row first, then puts the square cells in each row.
   for (let j = 0; j < size; j++) {
-    //Made the code shorter-- realized that I can get a direct refernence from creating the object,
-    //rather than going out of my way using querySelector
     const currRow = document.createElement("div");
     currRow.setAttribute("class", "row");
     gridContainer.appendChild(currRow);
 
-    console.log(currRow);
     for (let k = 0; k < size; k++) {
       const cell = document.createElement("div");
       cell.setAttribute("class", "cell");
       currRow.appendChild(cell);
     }
+  }
+}
+
+function removeGrid() {
+  const allRows = document.querySelectorAll(".row");
+
+  for (let row of allRows) {
+    row.remove();
   }
 }
