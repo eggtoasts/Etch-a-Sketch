ece7421fb7bce11b4388fcaeabb8e33c3fe3ddb6
eggtoasts <129806552+eggtoasts@users.noreply.github.com>
2025-09-08 20:07:03 -0400

    Added a feature that lets users pick a color to draw with, and centered the layout. This patch contains a side bar that contains 4 colors (black, red, green, blue). Added flexbox to containers to center the grid.

--- script.js
+++ script.js
@@ -1,28 +1,80 @@
 const gridContainer = document.querySelector(".grid-container");
 const sizeSlider = document.querySelector(".slider");
 const sizeOfGridText = document.querySelector(".grid-size-text");
+const colorPicker = document.querySelector(".color-picker");
+
+const canDraw = true;
+
+const green = "rgb(79, 255, 77)";
+const red = "rgb(255, 78, 78)";
+const blue = "rgb(22, 150, 255)";
 
 //Default size.
 makeGrid(16);
 
 let sizeOfGrid = 16;
 
+//Our default color is black/
+let currentColor = `black`;
+
+const colors = [`black`, `green`, `red`, `blue`, `rainbow`];
+
+// Function that lets you change the color
+const colorOptions = document.querySelectorAll(".color");
+
+for (let color of colorOptions) {
+  //   console.log(color);
+
+  color.addEventListener("click", (event) => {
+    // console.log(event.target);
+    let selectedColor = event.target.id;
+    console.log(selectedColor);
+
+    switch (selectedColor) {
+      case `black`:
+        currentColor = "black";
+        break;
+      case `red`:
+        currentColor = "red";
+        break;
+      case `green`:
+        currentColor = "green";
+        break;
+      case `blue`:
+        currentColor = "blue";
+        break;
+      case `rainbow`:
+        currentColor = "rainbow";
+        break;
+    }
+
+    // switch ()
+  });
+}
+
+colorPicker.addEventListener("hover", (event) => {
+  console.log(event.target);
+});
+
 //When user moves the slider, the grid size moves to its respective size.
-sizeSlider.addEventListener("mousedown", (event) => {
-  let target = event.target;
-
-  let sizeInterval = setInterval(() => {
-    console.log("This is" + target.value);
-    updateSizeText(target.value);
-    makeGrid(target.value);
-  }, 100);
-
-  sizeSlider.addEventListener("mouseup", () => {
+// "mousedown onclick".split(" ").forEach(function(e){})
+[`mousedown`, `onclick`].forEach((e) => {
+  sizeSlider.addEventListener(e, (event) => {
     let target = event.target;
-    console.log(target.value);
 
-    clearInterval(sizeInterval);
-    updateSizeText(target.value);
+    let sizeInterval = setInterval(() => {
+      console.log("This is" + target.value);
+      updateSizeText(target.value);
+      makeGrid(target.value);
+    }, 50);
+
+    sizeSlider.addEventListener("mouseup", () => {
+      let target = event.target;
+      console.log(target.value);
+
+      clearInterval(sizeInterval);
+      updateSizeText(target.value);
+    });
   });
 });
 
@@ -65,7 +117,18 @@ function addCellEvents(cell) {
   cell.addEventListener("mouseover", (event) => {
     let target = event.target;
 
-    target.style["background-color"] = "black";
-    console.log(target);
+    console.log(currentColor);
+
+    if (currentColor == "black") {
+      target.style["background-color"] = "black";
+    } else if (currentColor == "red") {
+      target.style["background-color"] = red;
+    } else if (currentColor == "blue") {
+      target.style["background-color"] = blue;
+    } else if (currentColor == "green") {
+      target.style["background-color"] = green;
+    }
+
+    // console.log(target);
   });
 }
